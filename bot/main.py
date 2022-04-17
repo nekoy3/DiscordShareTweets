429780dcbbd9369ab6d117c253809b1df049cb2a
nekoy3 <nekoyama3.n@gmail.com>
2022-04-17 20:56:13 +0900

    config作成

--- bot/main.py
+++ bot/main.py
@@ -1,13 +1,26 @@
+import bot
 import discord
 
-bot = discord.Bot()
+b = discord.Bot()
 
-@bot.event
-async def on_ready():
-    print(f"We have logged in as {bot.user}")
+def main():
+    global b
 
-@bot.slash_command(guild_ids=[956435214824009748])
-async def hello(ctx):
-    await ctx.respond("Hello!")
+    @b.event
+    async def on_ready():
+        print(f"We have logged in as {b.user}")
 
-bot.run("token")
+    @b.slash_command(guild_ids=[956435214824009748])
+    async def hello(ctx):
+        await ctx.respond("Hello!")
+
+    b.run("token")
+    
+    try:
+        b.run('token')
+    except KeyboardInterrupt:
+        print("\nLogout")
+        b.logout()
+
+if __name__ == "__main__":
+    main()
